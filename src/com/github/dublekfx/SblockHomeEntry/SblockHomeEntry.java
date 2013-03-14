0585028bb46ea4b1fb8f331fd1b850b0bfea9cc2
Dublekfx <dublekfx@gmail.com>
2013-03-14 18:28:45 -0400

    Set up beginnings of project
    onEnable() and onDisable()
    created plugin.yml

---
+++ src/com/github/dublekfx/SblockHomeEntry/SblockHomeEntry.java
@@ -0,0 +1,16 @@
+package com.github.dublekfx.SblockHomeEntry;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SblockHomeEntry extends JavaPlugin {
+
+	@Override
+	public void onEnable()	{
+		
+	}
+	
+	@Override
+	public void onDisable()	{
+		
+	}
+}
